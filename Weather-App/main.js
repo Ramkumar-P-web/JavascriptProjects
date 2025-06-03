432038520618766a2c65c2fead04131b2638b4c4
Ramkumar-P-web <p.ramkumar44ram@gmail.com>
2025-06-03 21:52:03 +0530

    Dynamic page created app will update data based on weatger API

---
+++ Weather-App/main.js
@@ -0,0 +1,58 @@
+const apiKey = "dc3672873f360f852d514dde39997e0b";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="
+
+const input = document.querySelector('.search input');
+const searchBtn = document.querySelector('.search button');
+const weatherImg = document.getElementById('weatherImg');
+const home = document.querySelector('.home');
+const error = document.querySelector('.error');
+const weather = document.querySelector('.weather');
+
+
+async function currentWeather(cityName){
+     const weatherReport = await fetch(apiUrl+`${cityName}`+`&appid=${apiKey}`+`&units=metric`);
+    
+     if(weatherReport.status == 404){
+        home.style.display = "block";
+       error.style.display = "block";
+       weather.style.display = "none";
+     }else{
+    const data = await weatherReport.json();
+     
+    document.getElementById('description').innerHTML = data.weather[0].description;
+    document.querySelector('.temp h1').innerHTML = Math.floor(data.main.temp);
+    document.getElementById('cityname').innerHTML = data.name;
+    document.getElementById('cityname').style.color = "#fd8f01";
+    document.getElementById('humidityper').innerHTML = data.main.humidity + ' %';
+    document.getElementById('windspeed').innerHTML = Math.floor(data.wind.speed)  + ' M/S';
+
+     if(data.weather[0].main == "Clouds"){
+           weatherImg.src = "./images/cloudy.gif"
+     }else if(data.weather[0].main == "Clear"){
+           weatherImg.src = "./images/sun.gif"
+     }else if(data.weather[0].main == "Drizzle"){
+          weatherImg.src = "./images/drizzle.gif"
+     }else if(data.weather[0].main == "Rain"){
+           weatherImg.src = "./images/rain.gif"
+     }else if(data.weather[0].main == "thunderstorm"){
+           weatherImg.src = "./images/storm.gif"
+     }else if(data.weather[0].main == 'snow'){
+           weatherImg.src = "./images/snow.gif"
+     }
+     home.style.display = "none";
+     error.style.display = "none";
+     weather.style.display = "flex";
+     }
+     
+};
+
+searchBtn.addEventListener('click',()=>{
+    currentWeather(`${input.value}`)
+    input.value = "";
+});
+
+document.querySelector('.home button').addEventListener('click',()=>{
+    home.style.display = "none"
+    weather.style.display = "flex"
+    document.getElementById('cityname').innerHTML = "Enter city name to check current weather"
+    document.getElementById('cityname').style.color = "#fd6050"});
